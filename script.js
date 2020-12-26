2d4ef43e3f5e588e4179fdd3ab62a5a85c5f5142
Sai Prasad Nayak <saiprasadnayak6858@gmail.com>
2020-12-26 23:32:48 +0530

    feat:add sticky navbar

--- script.js
+++ script.js
@@ -30,4 +30,18 @@ function toggleMenu() {
     });
 
 })();
+$(document).ready(function() {
   
+  $(window).scroll(function () {
+      //if you hard code, then use console
+      //.log to determine when you want the 
+      //nav bar to stick.  
+      console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 80) {
+      $('header').addClass('navbar-fixed');
+    }
+    if ($(window).scrollTop() < 81) {
+      $('header').removeClass('navbar-fixed');
+    }
+  });
+})
