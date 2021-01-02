c4ca114aacaaee6b3a2b2e771bd77fbefcdfaf9d
Sai Prasad Nayak <saiprasadnayak6858@gmail.com>
2021-01-02 21:59:12 +0530

    feat:send mail onclick of button

--- script.js
+++ script.js
@@ -9,60 +9,15 @@ function toggleMenu() {
     menu.classList.add('hidden');
   }
 }
+function sendEmail() 
+{
+    window.location = "mailto:xyz@abc.com";
+}
 
 const isHidden = [true, true, true]
 const sections = [0,1,1,2,2,3,3,4,4]
 
 $(document).ready(function() {
-  // $.getJSON("assets/json/projects.json",(data)=>{
-  //   console.log(data);
-  //   data.forEach(e => {
-  //     $(`<div class="card effect effect-one">
-  //     <img src="/assets/images/Projects/${e.img}" />
-  //     <div class="tab-text">
-  //         <h2 class="pt-1">${e["Project name"]}</h2>
-  //         <p>${e["desc"].slice(0,300)+" ..."}</p>
-  //         <div class="icons-block">
-  //             <a href="#" class="social-icon-1"><i class="bx bxl-chrome"></i></a> <a href="${e['github']}" class="social-icon-2"><i class="bx bxl-github"></i></a>
-  //         </div>
-  //     </div>
-  // </div>`).appendTo("#grid0");
-  //   });
-  // })
-
-  // $.getJSON("assets/json/members.json",(data)=>{
-  //   console.log(data);
-  //   data.forEach(e => {
-    //   $(`<div class="card"> 
-    //   <div class="imgBx fill"> 
-    //     <img 
-    //       src="${e["Upload Photo"]}" 
-    //       alt=""
-    //       style=""
-    //     />
-    //   </div>
-    //   <div class="content">
-    //     <div class="contentBx">
-    //       <h3>${e.Name} <br /><span>${e.position}</span></h3>
-    //     </div>
-    //     <ul class="sci">
-    //       <li style="--i: 1">
-    //         <a href="${e["LinkedIn Profile"]}"><i class="bx bxl-linkedin" aria-hidden="true"></i></a>
-    //       </li>
-    //       <li style="--i: 2">
-    //         <a href="${e["Twitter Handle"]}"><i class="bx bxl-twitter" aria-hidden="true"></i></a>
-    //       </li>
-    //       <li style="--i: 3">
-    //         <a href="${e["Github Handle"]}"><i class="bx bxl-github" aria-hidden="true"></i></a>
-    //       </li>
-    //     </ul>
-    //   </div>
-    // </div>`).appendTo("#grid2");
-  //   });
-
-  // })
-
-
   $('#mob-menu li').on('click', function() {
     if($('#mob-menu').hasClass('hidden')){
       $('#mob-menu').removeClass('hidden').addClass('block');
