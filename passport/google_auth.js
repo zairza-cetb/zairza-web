5a18bb45b830b051de99cd8c9d42530a47940afe
Biswajit Patra <patrabiswajit133@gmail.com>
2021-03-14 19:26:22 +0530

    Add Google Authentication

--- passport/google_auth.js
+++ passport/google_auth.js
@@ -7,23 +7,71 @@ module.exports = function (passport) {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL: "/auth/google/cb/",
+        passReqToCallback: true,
       },
-      function (accessToken, refreshToken, profile, done) {
-        User.findOne({ googleId: profile.id }).then((currentUser) => {
-          if (currentUser) {
-            //if we already have a record with the given profile ID
-            done(null, currentUser);
-          } else {
-            //if not, create a new user
-            new User({
-              googleId: profile.id,
-            })
-              .save()
-              .then((newUser) => {
-                done(null, newUser);
-              });
-          }
-        });
+      function (req, token, refreshToken, profile, done) {
+        if (!req.user) {
+          User.findOne(
+            { email: profile.emails[0].value },
+            function (err, user) {
+              if (err) return done(err);
+
+              if (user) {
+                google_auth = user.third_party_auth
+                  .filter(
+                    (third_party) => third_party.provider_name == "google"
+                  )
+                  .pop();
+                if (!google_auth) {
+                  user.third_party_auth.push({
+                    provider_name: "google",
+                    provider_id: profile.id,
+                    provider_token: token,
+                  });
+
+                  user.save(function (err) {
+                    if (err) throw err;
+                    return done(null, user);
+                  });
+                }
+
+                return done(null, user);
+              } else {
+                var newUser = new User();
+                newUser.name = profile.displayName;
+                newUser.email = profile.emails[0].value;
+
+                newUser.third_party_auth.push({
+                  provider_name: "google",
+                  provider_id: profile.id,
+                  provider_token: token,
+                });
+
+                newUser.save(function (err) {
+                  if (err) throw err;
+                  return done(null, newUser);
+                });
+              }
+            }
+          );
+        } else {
+          // For users that are already authunticated through other methods
+          var user = req.user;
+
+          user.name = profile.displayName;
+          user.email = profile.emails[0].value;
+
+          user.third_party_auth.push({
+            provider_name: "google",
+            provider_id: profile.id,
+            provider_token: token,
+          });
+
+          user.save(function (err) {
+            if (err) throw err;
+            return done(null, user);
+          });
+        }
       }
     )
   );
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
 const checkUserLoggedIn = (req, res, next) => {
-  req.user ? next(): res.sendStatus(401);
-}
+  req.user ? next() : res.sendStatus(401);
+};
+
+router.get("/me", checkUserLoggedIn, function (req, res, next) {
+  res.send(req.user);
+});
 
 module.exports = router;
