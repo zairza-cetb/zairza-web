fd145ee6a20f9f5c3c86872707bdbc75dcf6eacd
saiprasadnayak6858 <saiprasadnayak6858@gmail.com>
2021-03-14 14:00:43 +0530

    feat:add email regex validation

--- public/js/auth.js
+++ public/js/auth.js
@@ -2,3 +2,20 @@ const toggleForm = () => {
   const container = document.querySelector(".container");
   container.classList.toggle("active");
 };
+
+// Email validate through regex
+function validateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+$("input[type='email']").on("change", function(){
+  $response = validateEmail($(this).val());
+  if(!$response){
+    $(this).addClass("border-2 border-red-500");
+    $(this).siblings("small").removeClass("hidden");
+  }else{
+    $(this).removeClass("border-2 border-red-500");
+    $(this).siblings("small").addClass("hidden");
+  }
+})
