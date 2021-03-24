a89921741e170822c2b0e00d0a3fa3a4b6d36bac
saiprasadnayak6858 <saiprasadnayak6858@gmail.com>
2021-03-25 00:28:10 +0530

    feat:add & integrate toast messages

---
+++ public/js/thirdPartyAuth.js
@@ -0,0 +1,46 @@
+// Auth popup
+let authWindow;
+
+window.addEventListener("message", function (e) {
+  if (e.data === "popup-done") {
+    showToast(200, "Successfull");
+    setTimeout(function () {
+      window.location.replace("/me");
+    }, 2000);
+  } else if (e.data === "popup-failed") {
+    showToast(400, "Bad request");
+    setTimeout(function () {
+      window.location.replace("/auth");
+    }, 2000);
+  }
+});
+
+// ThirdParty Signin
+function authenticate(provider_name) {
+  var width = 1366,
+    height = 768;
+  let link;
+  if (provider_name === "github") {
+    link = "/auth/github";
+  } else {
+    link = "/auth/google";
+  }
+  var w = window.outerWidth - width,
+    h = window.outerHeight - height;
+  var left = Math.round(window.screenX + w / 2);
+  var top = Math.round(window.screenY + h / 2.5);
+
+  authWindow = window.open(
+    link,
+    "Authenticate",
+    "width=" +
+      width +
+      ",height=" +
+      height +
+      ",left=" +
+      left +
+      ",top=" +
+      top +
+      ",toolbar=0,scrollbars=0,status=0,resizable=0,location=0,menuBar=0"
+  );
+}
---
+++ public/js/toast.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+  $(".toast__close").click(function (e) {
+    e.preventDefault();
+    var parent = $(this).parent(".toast");
+    parent.fadeOut("slow", function () {
+      $(this).remove();
+    });
+  });
+});
+
+// Show toast based on response message
+
+function showToast(status_code, message) {
+  if (status_code === 200) {
+    $(".toast__icon").html(`<svg
+    version="1.1"
+    class="toast__svg"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    x="0px"
+    y="0px"
+    viewBox="0 0 512 512"
+    style="enable-background: new 0 0 512 512"
+    xml:space="preserve"
+  >
+    <g>
+      <g>
+        <path
+          d="M504.502,75.496c-9.997-9.998-26.205-9.998-36.204,0L161.594,382.203L43.702,264.311c-9.997-9.998-26.205-9.997-36.204,0    c-9.998,9.997-9.998,26.205,0,36.203l135.994,135.992c9.994,9.997,26.214,9.99,36.204,0L504.502,111.7    C514.5,101.703,514.499,85.494,504.502,75.496z"
+        ></path>
+      </g>
+    </g>
+  </svg>`);
+    $(".toast__type").text("Success");
+    $(".toast__message").text(message);
+    $('#toast').addClass('toast--green')
+  } else {
+    $(".toast__icon").html(`<img src="./images/auth/cancel.svg" alt="" style="color:#fff">`);
+    $(".toast__type").text(status_code);
+    $(".toast__message").text(message);
+    $('#toast').addClass('toast--yellow')
+  }
+  $("#toast").fadeIn("slow", () => {
+    $(this).toggleClass("hidden")
+  });
+  setTimeout(() => {
+    $("#toast").fadeOut("slow", function () {
+      $(this).remove();
+    });
+  },2000)
+}
+
+// showToast(400, 'lorem10asudhfuiewhfiuahdflaius')
