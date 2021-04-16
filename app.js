3902f358bea6299cd72ed25b98dc09ab3ad4b95f
Biswajit Patra <patrabiswajit133@gmail.com>
2021-04-16 23:58:43 +0530

    Remove passport authentication

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
-const passport = require("passport");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const session = require("express-session");
-const MongoDBStore = require("connect-mongodb-session")(session);
 
-const auth_routes = require("./routes/auth");
-const passport_setup = require("./passport/setup");
 const errorHandler = require("./error_handlers/errorHandler");
 
 const indexRouter = require("./routes/index");
@@ -25,17 +20,8 @@ const connection = mongoose
   })
   .then(console.log(`MongoDB connected`))
   .catch((err) => console.log(err));
-admin_panel(app, connection);
 
-// For session storage
-var store = new MongoDBStore({
-  uri: process.env.MONGO_URI,
-  collection: "Sessions",
-});
-// Catch errors
-store.on("error", function (error) {
-  console.log(error);
-});
+admin_panel(app, connection);
 
 app.set("view engine", "ejs");
 app.use(logger("dev"));
@@ -46,24 +32,9 @@ app.use(
   express.static(path.join(__dirname, "public"), { extensions: ["html"] })
 );
 
-// Passport middleware
-passport_setup(passport);
-
-app.use(
-  session({
-    secret: process.env.SECRET,
-    store: store,
-    resave: true,
-    saveUninitialized: true,
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
-
 app.use("/", indexRouter);
 app.use("/user", userRouter);
 
-auth_routes(app, passport);
 
 app.get("*", function (req, res) {
   res.status(404).sendFile(path.join(__dirname, "public", "404.html"));
--- models/Users.js
+++ models/Users.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
 const ValidRegNos = require("./ValidRegNos");
-const ThirdPartyProviderSchema = require("./ThirdPartyAuth");
 
 const UserSchema = new mongoose.Schema(
   {
@@ -21,9 +19,6 @@ const UserSchema = new mongoose.Schema(
       type: Number,
       unique: true,
       sparse: true,
-    },
-    password: {
-      type: String,
     },
     wing: {
       type: String,
@@ -49,7 +44,6 @@ const UserSchema = new mongoose.Schema(
         "Chemistry",
       ],
     },
-    third_party_auth: [ThirdPartyProviderSchema],
     newsletter_subscription: {
       applied: { type: Boolean, default: false },
       applied_at: Date,
@@ -67,17 +61,6 @@ const UserSchema = new mongoose.Schema(
     timestamps: { createdAt: "createdAt", updatedAt: "updatedAt" },
   }
 );
-
-// generating a hash
-UserSchema.methods.generateHash = function (password) {
-  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
-
-// checking if password is valid
-UserSchema.methods.validPassword = function (password) {
-  if (!this.password) return false;
-  return bcrypt.compareSync(password, this.password);
-};
 
 // checking if registration no is valid
 UserSchema.methods.checkValidRegistrationNo = async function (registration_no) {
