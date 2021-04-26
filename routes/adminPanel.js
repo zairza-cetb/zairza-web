729ebafab2a95768a2c9f8484e910715173937c5
Biswajit Patra <patrabiswajit133@gmail.com>
2021-04-26 16:16:16 +0530

    Add logs db

--- routes/adminPanel.js
+++ routes/adminPanel.js
@@ -1,11 +1,35 @@
 const AdminBro = require("admin-bro");
+const { ACTIONS } = require("admin-bro");
 const AdminBroExpress = require("@admin-bro/express");
 const AdminBroMongoose = require("@admin-bro/mongoose");
+const express = require("express");
 const User = require("../models/Users");
 const ValidRegNos = require("../models/ValidRegNos");
+const ChangeLog = require("../models/ChangeLog");
 const checkIfAuthenticated = require("../firebase/firebaseCheckAuth");
 
-const express = require("express");
+const createLog = async (originalResponse, request, context) => {
+  if (
+    (request.method === "post" &&
+      originalResponse.record &&
+      !Object.keys(originalResponse.record.errors).length) ||
+    context.action.name === "delete"
+  ) {
+    await ChangeLog.create({
+      action: context.action.name,
+      userId: request.user,
+      resource: context.resource.id(),
+      recordId: context.record && context.record.id(),
+    });
+  }
+  return originalResponse;
+};
+
+if (process.env.NODE_ENV == "development") {
+  ACTIONS.edit.after = createLog;
+  ACTIONS.delete.after = createLog;
+  ACTIONS.new.after = createLog;
+}
 
 module.exports = (app, mongoose_connection) => {
   AdminBro.registerAdapter(AdminBroMongoose);
