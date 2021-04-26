a6be9fda9d3daa837b362c965880c8c25391fa01
Biswajit Patra <patrabiswajit133@gmail.com>
2021-04-26 12:41:43 +0530

    Add new api for newsletter

--- routes/user.js
+++ routes/user.js
@@ -12,30 +12,17 @@ const editableFields = new Set([
   "branch",
 ]);
 
-// Check user is authenticated or not
-router.use(checkIfAuthenticated);
-
-router.get("/me", function (req, res, next) {
+router.get("/me", checkIfAuthenticated, function (req, res, next) {
   res.json(req.user);
 });
 
-router.put("/edit", async function (req, res, next) {
+router.put("/edit", checkIfAuthenticated, function (req, res, next) {
   if (!req.user.registrationNo && !req.body.registrationNo) {
     return res
       .status(403)
       .json({ status: "fail", message: "Registration number is required" });
-  } else if (
-    req.body.registrationNo &&
-    (await req.user.checkValidRegistrationNo(req.body.registrationNo)) == false
-  ) {
-    return res.status(403).json({
-      status: "fail",
-      message:
-        "Your registration number is not registered at Zairza. Please contact us to register you at Zairza",
-    });
   } else {
     // Check for editable fields fields only
-
     var nonEditableFields = Object.keys(req.body).filter(
       (x) => !editableFields.has(x)
     );
@@ -46,26 +33,39 @@ router.put("/edit", async function (req, res, next) {
         .json({ status: "fail", message: "Accessing unknown fields" });
     }
 
-    User.findByIdAndUpdate(req.user.id, req.body, { new: true })
-      .then((user) => {
+    User.findByIdAndUpdate(
+      req.user.id,
+      req.body,
+      { new: true },
+      async function (err, user) {
+        if (err) {
+          return next(err);
+        }
         if (!user) {
           return res.status(404).json({
             message: "No user found",
           });
         }
-        if (user.role == "restricted") {
-          user.role = "user";
-          user.save();
+
+        if (req.body.registrationNo) {
+          if (
+            user.role === "restricted" &&
+            (await req.user.checkValidRegistrationNo(
+              req.body.registrationNo
+            )) === true
+          ) {
+            user.role = "user";
+            user.save();
+          }
         }
+
         res.status(200).json({ status: "success", user });
-      })
-      .catch((err) => {
-        return next(err);
-      });
+      }
+    );
   }
 });
 
-router.delete("/", function (req, res, next) {
+router.delete("/", checkIfAuthenticated, function (req, res, next) {
   User.findByIdAndRemove(req.user.id)
     .then((user) => {
       if (!user) {
@@ -89,6 +89,37 @@ router.delete("/", function (req, res, next) {
     .catch((err) => {
       return next(err);
     });
+});
+
+const isRoleUser = [
+  checkIfAuthenticated,
+  (req, res, next) => {
+    if (req.user.role === "restricted") {
+      const error = new Error("User privilage required");
+      error.status = "fail";
+      error.statusCode = 403;
+      return next(error);
+    }
+    next();
+  },
+];
+
+router.post("/newsletter", isRoleUser, function (req, res, next) {
+  User.findByIdAndUpdate(
+    req.user.id,
+    { newsletterSubscription: req.body.newsletterSubscription },
+    function (err, user) {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        return res.status(404).json({
+          message: "No user found",
+        });
+      }
+      return res.json({ status: "success" });
+    }
+  );
 });
 
 module.exports = router;
