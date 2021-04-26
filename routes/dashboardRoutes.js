6f02dc52103425cf2ef44bb489d635d97903f7ac
Biswajit Patra <patrabiswajit133@gmail.com>
2021-04-26 23:42:07 +0530

    Add members Count to dashboard page

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -2,10 +2,24 @@ const express = require("express");
 const router = express.Router();
 const checkIfAuthenticated = require("../firebase/firebaseCheckAuth");
 const fetch = require("node-fetch");
+const ValidRegNos = require("../models/ValidRegNos");
 
 /* GET dashboard page. */
 router.get("/me", checkIfAuthenticated, function (req, res, next) {
-  res.render("pages/dashboard/index", { user: req.user });
+  ValidRegNos.count({}, function (err, count) {
+    if (err) {
+      return next(err);
+    }
+    fetch("https://api.github.com/users/zairza-cetb/repos?sort=updated_at")
+      .then((res) => res.json())
+      .then((data) => {
+        return res.render("pages/dashboard/index", {
+          user: req.user,
+          projectsCount: data.length,
+          membersCount: count,
+        });
+      });
+  });
 });
 
 /* GET projects page. */
@@ -13,7 +27,10 @@ router.get("/projects", checkIfAuthenticated, function (req, res, next) {
   fetch("https://api.github.com/users/zairza-cetb/repos?sort=updated_at")
     .then((res) => res.json())
     .then((data) => {
-      return res.render("pages/dashboard/projects", { projects : data , user: req.user });
+      return res.render("pages/dashboard/projects", {
+        projects: data,
+        user: req.user,
+      });
     });
 });
 
