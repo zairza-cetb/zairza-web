635abec189be912cb6815e91778e5e0df29f2650
StrangeNoob <sipun63427452@gmail.com>
2021-05-03 13:27:34 +0530

    rfac: Add Timer

--- public/js/script.js
+++ public/js/script.js
@@ -95,6 +95,42 @@ $(document).ready(function () {
     $(element).click(() => fullpage_api.moveTo(navSectionMap[index]));
   });
 
+  $(document).ready(function() {
+    function updateTimer() {
+      future  = Date.parse("May 14, 2021 10:30:00");
+      now     = new Date();
+      diff    = future - now;
+      days  = Math.floor( diff / (1000*60*60*24) );
+      hours = Math.floor( diff / (1000*60*60) );
+      mins  = Math.floor( diff / (1000*60) );
+      secs  = Math.floor( diff / 1000 );
+      d = days;
+      h = hours - days  * 24;
+      m = mins  - hours * 60;
+      s = secs  - mins  * 60;
+
+      d = days;
+      h = hours - days  * 24;
+      m = mins  - hours * 60;
+      s = secs  - mins  * 60;
+      if(diff<0){
+       d=0;
+       h=0;
+       m=0;
+       s=0; 
+      }
+      document.getElementById("timer")
+        .innerHTML =
+          '<div>' + d + '<span>days</span></div>' +
+          '<div>' + h + '<span>hours</span></div>' +
+          '<div>' + m + '<span>minutes</span></div>' +
+          '<div>' + s + '<span>seconds</span></div>' ;
+    }   
+    setInterval(updateTimer(), 1000 );
+  });
+
+  
+
   // Newsletter cookie
   //   let visited = $.cookie("visited");
   //   if (visited != "yes") {
