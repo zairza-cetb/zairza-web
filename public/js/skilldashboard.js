60d8810fe535d516a14cfc5971304ef2607094fb
Shuvam Kumar Panda <shuvamkumar2001@gmail.com>
2021-06-16 17:45:28 +0530

    feat:add skill++ dashboard

---
+++ public/js/skilldashboard.js
@@ -0,0 +1,44 @@
+const faq = [
+  {
+    question: "Week 1: Resources",
+    answer: [
+      "Bacon ipsum dolor amet boudin hamburger jerky spare ribs, bacon leberkas beef ribs sausage turkey pancetta tenderloin chicken.",
+      "Meatball landjaeger turducken. Bacon bresaola tenderloin cow rump pork chop.",
+    ],
+  },
+  {
+    question: "Week 1: Task ",
+    answer: [
+      "Boudin sausage hamburger tenderloin beef chislic prosciutto pancetta. Beef tongue pork meatloaf.",
+      "Chicken pork chop turducken ground round. Shank bresaola burgdoggen short loin ham hock ham. Boudin tri-tip swine drumstick strip steak tail.",
+    ],
+  },
+  {
+    question: "Week 2: Resources",
+    answer: [
+      "Salami filet mignon strip steak venison rump chicken bresaola. Shank flank tongue ribeye. Beef pork chop sirloin venison chicken jowl.",
+      "Doner corned beef kielbasa beef ribs ground round cow salami swine.",
+    ],
+  },
+  {
+    question: "Week 2: Task",
+    answer: [
+      "Bacon ipsum dolor amet boudin hamburger jerky spare ribs, bacon leberkas beef ribs sausage turkey pancetta tenderloin chicken.",
+      "Meatball landjaeger turducken. Bacon bresaola tenderloin cow rump pork chop.",
+    ],
+  },
+  {
+    question: "Week 3: Resources",
+    answer: [
+      "Boudin sausage hamburger tenderloin beef chislic prosciutto pancetta. Beef tongue pork meatloaf.",
+      "Chicken pork chop turducken ground round. Shank bresaola burgdoggen short loin ham hock ham. Boudin tri-tip swine drumstick strip steak tail.",
+    ],
+  },
+  {
+    question: "Week 3: Task",
+    answer: [
+      "Salami filet mignon strip steak venison rump chicken bresaola. Shank flank tongue ribeye. Beef pork chop sirloin venison chicken jowl.",
+      "Doner corned beef kielbasa beef ribs ground round cow salami swine.",
+    ],
+  },
+];
