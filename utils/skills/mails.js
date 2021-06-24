87a4321fff6d9326cc9fd40515e76d81cd83fd09
Biswajit Patra <patrabiswajit133@gmail.com>
2021-06-24 23:37:56 +0530

    bfix: Event upload resolved

--- utils/skills/mails.js
+++ utils/skills/mails.js
@@ -9,9 +9,7 @@ attachment = fs.readFileSync(pathToAttachment).toString("base64");
 module.exports = (agenda) => {
 	// Send mail to all skills++ registered users that week's task has been unlocked
 	agenda.define("week start", async function (job, done) {
-		DomainRegistrations.find({
-			user: mongoose.Types.ObjectId("60d036543a802d148c58abf6"),
-		})
+		DomainRegistrations.find({})
 			.populate("user")
 			.populate("domain")
 			.exec(function (err, results) {
@@ -24,20 +22,12 @@ module.exports = (agenda) => {
 					}
 					sendMail({
 						email: results[i].user.email,
-						templateId: "d-adca85aca55c44f4bf8287beb4870490",
+						templateId: "d-29d30da4f328411c98aba9923868d1f8",
 						dynamic_template_data: {
 							name: results[i].user.name.split(" ")[0],
 							domain: results[i].domain.name,
 							process: `week start ${job.attrs.data.weekNo}`,
 						},
-						attachments: [
-							{
-								content: attachment,
-								filename: "my_resume_final_.pdf",
-								type: "application/pdf",
-								disposition: "attachment",
-							},
-						],
 					});
 				}
 				done();
@@ -45,22 +35,20 @@ module.exports = (agenda) => {
 	});
 	// Send mail to all users that registration process for skills++ has started
 	agenda.define("skill++ registration start", async function (job) {
-        // Users.find({ role: { $ne: "restricted" } }, function (err, users) {
-    // try{
-        Users.find( {
-            _id: mongoose.Types.ObjectId("60d036543a802d148c58abf6"),
-        }, function (err, users) {
-            console.log("hello start");
-            if (err) {
-                throw err;
-            }
+		Users.find({ role: { $ne: "restricted" },
+			// _id:mongoose.Schema.ObjectId("60d036543a802d148c58abf6") 
+		}, function (err, users) {
+			if (err) {
+				throw err;
+			}
 			for (let i = 0; i < users.length; i++) {
 				if (users[i].name === undefined) {
 					users[i].name = "";
 				}
 				sendMail({
 					email: users[i].email,
-					templateId: "d-adca85aca55c44f4bf8287beb4870490",
+					templateId: "d-29d30da4f328411c98aba9923868d1f8",
+					from: "info@zairza.in",
 					dynamic_template_data: {
 						name: users[i].name.split(" ")[0],
 						process: "skill++ registration start",
@@ -76,8 +64,5 @@ module.exports = (agenda) => {
 				});
 			}
 		});
-    // }catch (e) {
-    //     console.log(e);
-    // }
 	});
 };
