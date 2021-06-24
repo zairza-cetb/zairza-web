730e85b608199fe8cb1f4b85ad6b47b06dc62305
Biswajit Patra <patrabiswajit133@gmail.com>
2021-06-25 00:53:44 +0530

    bfix: Add brochure pdf to email

--- utils/skills/mails.js
+++ utils/skills/mails.js
@@ -2,15 +2,16 @@ const DomainRegistrations = require("../../models/skills/DomainRegistrations");
 const Users = require("../../models/Users");
 const sendMail = require("../sendMail");
 
-const http = require('http'); // or 'https' for https:// URLs
+const http = require('http');
 const fs = require('fs');
 
-// const file = fs.createWriteStream("file.jpg");
-// const request = http.get("http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg", function(response) {
-//   response.pipe(file);
-// });
-// pathToAttachment = `./temp/documents.jpg`;
-// attachment = fs.readFileSync(pathToAttachment).toString("base64");
+const file = fs.createWriteStream("temp/brochure.pdf");
+const request = http.get("http://zairza-website.s3.ap-south-1.amazonaws.com/miscellaneous/Skills%2B%2B+Brochure.pdf", function(response) {
+  response.pipe(file);
+});
+pathToAttachment = `./temp/brochure.pdf`;
+attachment = fs.readFileSync(pathToAttachment).toString("base64");
+
 
 module.exports = (agenda) => {
 	// Send mail to all skills++ registered users that week's task has been unlocked
@@ -41,38 +42,34 @@ module.exports = (agenda) => {
 	});
 	// Send mail to all users that registration process for skills++ has started
 	agenda.define("skill++ registration start", async function (job) {
-		Users.find(
-			{
-				role: { $ne: "restricted" },
-				// _id:mongoose.Schema.ObjectId("60d036543a802d148c58abf6")
-			},
-			function (err, users) {
-				if (err) {
-					throw err;
-				}
-				for (let i = 0; i < users.length; i++) {
-					if (users[i].name === undefined) {
-						users[i].name = "";
-					}
-					sendMail({
-						email: users[i].email,
-						templateId: "d-29d30da4f328411c98aba9923868d1f8",
-						from: "info@zairza.in",
-						dynamic_template_data: {
-							name: users[i].name.split(" ")[0],
-							process: "skill++ registration start",
-						},
-						attachments: [
-							// {
-							// 	content: attachment,
-							// 	filename: "brochure.pdf",
-							// 	type: "application/pdf",
-							// 	disposition: "attachment",
-							// },
-						],
-					});
-				}
+		Users.find({ role: { $ne: "restricted" },
+			// _id:mongoose.Schema.ObjectId("60d036543a802d148c58abf6") 
+		}, function (err, users) {
+			if (err) {
+				throw err;
 			}
-		);
+			for (let i = 0; i < users.length; i++) {
+				if (users[i].name === undefined) {
+					users[i].name = "";
+				}
+				sendMail({
+					email: users[i].email,
+					templateId: "d-29d30da4f328411c98aba9923868d1f8",
+					from: "info@zairza.in",
+					dynamic_template_data: {
+						name: users[i].name.split(" ")[0],
+						process: "skill++ registration start",
+					},
+					attachments: [
+						{
+							content: attachment,
+							filename: "brochure.pdf",
+							type: "application/pdf",
+							disposition: "attachment",
+						},
+					],
+				});
+			}
+		});
 	});
 };
