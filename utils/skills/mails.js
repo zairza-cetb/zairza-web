b1f81bc1cc841a48f808fd9308792d7d22f97ec6
Biswajit Patra <patrabiswajit133@gmail.com>
2021-06-29 23:58:51 +0530

    rfac: Removed subtasks from domain

--- utils/skills/mails.js
+++ utils/skills/mails.js
@@ -2,20 +2,6 @@ const DomainRegistrations = require("../../models/skills/DomainRegistrations");
 const Users = require("../../models/Users");
 const sendMail = require("../sendMail");
 
-const https = require("https");
-const fs = require("fs");
-
-// fs.mkdir("temp", () => {});
-// const file = fs.createWriteStream("temp/brochure.pdf");
-// const request = https.get(
-// 	"https://zairza-website.s3.ap-south-1.amazonaws.com/miscellaneous/Skills%2B%2B+Brochure+(1).pdf",
-// 	function (response) {
-// 		response.pipe(file);
-// 	}
-// );
-pathToAttachment = `./temp/brochure.pdf`;
-// attachment = fs.readFileSync(pathToAttachment).toString("base64");
-
 module.exports = (agenda) => {
 	// Send mail to all skills++ registered users that week's task has been unlocked
 	agenda.define("week start", async function (job, done) {
@@ -32,52 +18,16 @@ module.exports = (agenda) => {
 					}
 					sendMail({
 						email: results[i].user.email,
-						templateId: "d-29d30da4f328411c98aba9923868d1f8",
+						templateId: "d-2ffad5b69c0f4a04a303ec22a4cebc20",
 						from: "info@zairza.in",
 						dynamic_template_data: {
 							name: results[i].user.name.split(" ")[0],
 							domain: results[i].domain.name,
-							process: `week start ${job.attrs.data.weekNo}`,
+							weekNo: job.attrs.data.weekNo
 						},
 					});
 				}
 				done();
 			});
-	});
-	// Send mail to all users that registration process for skills++ has started
-	agenda.define("skill++ registration start", async function (job) {
-		Users.find(
-			{
-				role: { $ne: "restricted" },
-				// _id:mongoose.Schema.ObjectId("60d036543a802d148c58abf6")
-			},
-			function (err, users) {
-				if (err) {
-					throw err;
-				}
-				for (let i = 0; i < users.length; i++) {
-					if (users[i].name === undefined) {
-						users[i].name = "";
-					}
-					sendMail({
-						email: users[i].email,
-						templateId: "d-29d30da4f328411c98aba9923868d1f8",
-						from: "info@zairza.in",
-						dynamic_template_data: {
-							name: users[i].name.split(" ")[0],
-							process: "skill++ registration start",
-						},
-						attachments: [
-							{
-								content: attachment,
-								filename: "brochure.pdf",
-								type: "application/pdf",
-								disposition: "attachment",
-							},
-						],
-					});
-				}
-			}
-		);
 	});
 };
