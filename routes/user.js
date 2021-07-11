e8625baf336bbac59ec476a60f5bf6e97b7a72bf
Shuvam Kumar Panda <shuvamkumar2001@gmail.com>
2021-07-11 20:13:29 +0530

    ptch: link changes

--- routes/user.js
+++ routes/user.js
@@ -15,76 +15,86 @@ const editableFields = new Set([
   "branch",
 ]);
 
-router.get("/me", checkIfAuthenticated, function (req, res, next) {
+router.get("/home", checkIfAuthenticated, function (req, res, next) {
   res.json(req.user);
 });
 
-router.put("/edit", checkIfAuthenticated, uploadProfileImage.single("profileImage"), function (req, res, next) {
-  if (!req.user.registrationNo && !req.body.registrationNo) {
-    return res
-      .status(403)
-      .json({ status: "fail", message: "Registration number is required" });
-  } else {
-    // Check for editable fields fields only
-    var nonEditableFields = Object.keys(req.body).filter(
-      (x) => !editableFields.has(x)
-    );
-
-    if (nonEditableFields.length > 0) {
+router.put(
+  "/edit",
+  checkIfAuthenticated,
+  uploadProfileImage.single("profileImage"),
+  function (req, res, next) {
+    if (!req.user.registrationNo && !req.body.registrationNo) {
       return res
-        .status(401)
-        .json({ status: "fail", message: "Accessing unknown fields" });
-    }
+        .status(403)
+        .json({ status: "fail", message: "Registration number is required" });
+    } else {
+      // Check for editable fields fields only
+      var nonEditableFields = Object.keys(req.body).filter(
+        (x) => !editableFields.has(x)
+      );
 
-    if (req.file){
-      if(process.env.NODE_ENV === "production"){
-        req.body.profileImage = req.file.location;
-      }else{
-        req.body.profileImage = req.protocol + "://" + req.get('host') + "/" + req.file.path.substring(7);
+      if (nonEditableFields.length > 0) {
+        return res
+          .status(401)
+          .json({ status: "fail", message: "Accessing unknown fields" });
       }
-    }
-    User.findByIdAndUpdate(
-      req.user.id,
-      req.body,
-      { new: true },
-      async function (err, user) {
-        if (err) {
-          return next(err);
-        }
-        if (!user) {
-          return res.status(404).json({
-            message: "No user found",
-          });
-        }
 
-        if (req.body.registrationNo) {
-          if (
-            user.role === "restricted" &&
-            (await req.user.checkValidRegistrationNo(
-              req.body.registrationNo
-            )) === true
-          ) {
-            user.role = "user";
-            user.save();
-          } else if (
-            (await req.user.checkValidRegistrationNo(
-              req.body.registrationNo
-            )) === false
-          ) {
-            user.role = "restricted";
-            user.save();
+      if (req.file) {
+        if (process.env.NODE_ENV === "production") {
+          req.body.profileImage = req.file.location;
+        } else {
+          req.body.profileImage =
+            req.protocol +
+            "://" +
+            req.get("host") +
+            "/" +
+            req.file.path.substring(7);
+        }
+      }
+      User.findByIdAndUpdate(
+        req.user.id,
+        req.body,
+        { new: true },
+        async function (err, user) {
+          if (err) {
+            return next(err);
           }
+          if (!user) {
+            return res.status(404).json({
+              message: "No user found",
+            });
+          }
+
+          if (req.body.registrationNo) {
+            if (
+              user.role === "restricted" &&
+              (await req.user.checkValidRegistrationNo(
+                req.body.registrationNo
+              )) === true
+            ) {
+              user.role = "user";
+              user.save();
+            } else if (
+              (await req.user.checkValidRegistrationNo(
+                req.body.registrationNo
+              )) === false
+            ) {
+              user.role = "restricted";
+              user.save();
+            }
+          }
+          let message = null;
+          if (user.role === "restricted") {
+            message =
+              "Your registration number is not registered at Zairza. Please contact us to register you at Zairza";
+          }
+          res.status(200).json({ status: "success", user, message });
         }
-        let message = null;
-        if (user.role === "restricted") {
-          message =
-            "Your registration number is not registered at Zairza. Please contact us to register you at Zairza";
-        }
-        res.status(200).json({ status: "success", user, message });
-      }
-    );
+      );
+    }
   }
-});
+);
 
 router.delete("/", checkIfAuthenticated, function (req, res, next) {
   User.findByIdAndRemove(req.user.id)
@@ -145,7 +155,7 @@ router.post("/newsletter", isRoleUser, function (req, res, next) {
   );
 });
 
-router.post("/unsubscribe_newsletter", function(req, res, next) {
+router.post("/unsubscribe_newsletter", function (req, res, next) {
   User.findByIdAndUpdate(
     req.body.userId,
     { newsletterSubscription: false },
