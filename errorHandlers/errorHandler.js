9e530a23f25192b1276276b2313b7d84ca19dbd9
Biswajit <patrabiswajit133@gmail.com>
2021-07-27 15:29:12 +0530

    Update errorHandler.js

--- errorHandlers/errorHandler.js
+++ errorHandlers/errorHandler.js
@@ -78,7 +78,7 @@ const showAccessDeniedPage = (err, req, res) => {
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
-
+  console.log(err);
   if (!req.url.includes("/api/")) {
     if (err.codePrefix == "auth") {
       return redirectAuthPage(err, req, res);
